d02feef40fada378e630fcc0a7d68c05fca3523c
MohamedElmdary <engm5081@gmail.com>
2020-04-09 18:38:44 +0200

    made some changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,10 @@ export class AppService {
     const browser = await puppeteer.launch({
       headless,
       timeout: 0,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox', 
+        '--disable-setuid-sandbox'
+      ],
     });
     const page = await browser.newPage();
     return { browser, page };
