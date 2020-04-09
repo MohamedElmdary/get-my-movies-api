044965bae890c3125fcba688cdc621d9cadb966b
MohamedElmdary <engm5081@gmail.com>
2020-04-09 14:57:25 +0200

    hotfix-v1.01 - heroku deploy problem

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,11 @@ import * as puppeteer from 'puppeteer';
 @Injectable()
 export class AppService {
   async launchBrowser(headless = true) {
-    const browser = await puppeteer.launch({ headless, timeout: 0 });
+    const browser = await puppeteer.launch({
+      headless,
+      timeout: 0,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     return { browser, page };
   }
