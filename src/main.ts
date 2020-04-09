aac2e311b538da1c35f14ddefce098c61e129ca7
MohamedElmdary <engm5081@gmail.com>
2020-04-09 08:14:52 +0200

    added helmet and compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(helmet()).use(compression());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
