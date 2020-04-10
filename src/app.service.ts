ab9ec39a64ae6fa98048c632a4f5c80ccd7264ca
MohamedElmdary <engm5081@gmail.com>
2020-04-10 15:51:21 +0200

    added user agent

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,12 +7,12 @@ export class AppService {
     const browser = await puppeteer.launch({
       headless,
       timeout: 0,
-      args: [
-        '--no-sandbox', 
-        '--disable-setuid-sandbox'
-      ],
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36',
+    );
     return { browser, page };
   }
 }
